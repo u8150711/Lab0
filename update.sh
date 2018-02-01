cbd36a33fe03a0f951906c19587393c445fd6ba2
loshca <u121032@edu.miet.ru>
2018-02-01 10:40:44 +0300

    "Yes" flag was added
    GCC was added. In the case of wrong environment.
    Swap order between update and upgrade.
    Change indent by clang-format.

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-sudo apt-get upgrade
-sudo apt-get update
-sudo apt-get install nano
-sudo apt-get install indent
-sudo apt-get install iputils-ping
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install nano
+sudo apt -y install clang-format
+sudo apt -y install iputils-ping
+sudo apt -y install gcc
